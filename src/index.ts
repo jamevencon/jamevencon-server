c68ffae868dfd1aebffa5dc5afe61d10712d7123
YEAHx4 <developerstarlight@gmail.com>
2022-12-15 21:06:31 +0900

    ping req

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { ping, root } from "./routes/common";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5656;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+export type HttpHandler = (req: Request, res: Response) => any;
+
+const get = (url: string, handler: HttpHandler) => {
+  app.get(url, handler);
+  console.log(`[GET] ${url}`);
+};
+
+const post = (url: string, handler: HttpHandler) => {
+  app.post(url, handler);
+  console.log(`[POST] ${url}`);
+};
+
+get("/", root);
+get("/ping", ping);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
